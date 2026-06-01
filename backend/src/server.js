4004ff167bbabbbe4152d8cb6fa334266886bd99
cramesm <cramesm@students.national-u.edu.ph>
2026-06-01 19:34:19 +0800

    Support CLOUDINARY_URL env variable

--- backend/src/server.js
+++ backend/src/server.js
@@ -63,16 +63,21 @@ if (!JWT_SECRET) {
 }
 
 const cloudinaryEnabled = Boolean(
-  CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET,
+  process.env.CLOUDINARY_URL || (CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET),
 );
-
 if (cloudinaryEnabled) {
-  cloudinary.config({
-    cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
-    secure: true,
-  });
+  if (process.env.CLOUDINARY_URL) {
+    // The Cloudinary SDK automatically picks up process.env.CLOUDINARY_URL
+    // We just need to make sure we don't override it with empty values.
+    cloudinary.config(true);
+  } else {
+    cloudinary.config({
+      cloud_name: CLOUDINARY_CLOUD_NAME,
+      api_key: CLOUDINARY_API_KEY,
+      api_secret: CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+  }
 }
 
 const app = express();
